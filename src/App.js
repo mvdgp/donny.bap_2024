c2346fbf372d20674253d3d9f73110bc78dc5f68
mvdgp <mvandraanen@hotmail.com>
2024-12-13 18:03:20 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        App header
+      </header>
+      <main className="App-body">
+        App body
+      </main>
+    </div>
+  );
+}
+
+export default App;
