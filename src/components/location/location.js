ad0f42b5d76c96df9d91f23659979f01218244d2
mvdgp <mvandraanen@hotmail.com>
2026-02-04 18:16:44 +0100

    Temporarily closed

--- src/components/location/location.js
+++ src/components/location/location.js
@@ -14,7 +14,7 @@ const Location = () => {
             </div>
             <div className="location-info">
                 <p className="address">Tt. Melaniaweg 7<br/>1033 ST, Amsterdam</p><br/>
-                <p className="sat-sun"><b>Every Thursday to Sunday</b><br/>12.00 - 16.00</p>
+                <p className="sat-sun"><b>Temporarily closed!</b></p>
             </div>
         </div>
     );
